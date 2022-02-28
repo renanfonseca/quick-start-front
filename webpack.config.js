28eb5cd662405eb59a4d224ca2c05a939d47705d
Renan Fonseca <marcos.renan.cf@gmail.com>
2022-02-28 19:43:46 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/assets/js/main.js',
+    output: {
+        path : path.resolve(__dirname, 'public', 'assets', 'js'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.m?js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+};
